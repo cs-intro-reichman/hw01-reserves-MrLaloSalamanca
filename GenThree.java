99660b7d5b433f864f166d8366465d41397e0f34
MrLaloSalamanca <shahaf.lalo@post.runi.ac.il>
2024-02-04 19:52:40 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,20 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
+
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+			int min = Integer.parseInt(args[0]);
+			int max = Integer.parseInt(args[1]);
+			int a = ((int)((Math.random() * (max-min+1) + min)));
+			int b = ((int)((Math.random() * (max-min+1) + min)));
+			int c = ((int)((Math.random() * (max-min+1) + min)));
+			int d = Math.min(a,b);
+			min = Math.min(c,d);
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println("The minimal generated number was " + min);
+			
+			
+			
+			
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,11 @@
-/*
- * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
- * This is known as the Triangle Inequality Theorem. 
- * Write a program that tests if three given integers form a triangle.
- */ 
+
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		int z = Integer.parseInt(args[2]);
+		boolean b = (x+y>z) && (x+z>y) && (y+z>x);
+		System.out.println(x + ", " + y + ", " + z + " : " + b);		
+		
 	}
 }
